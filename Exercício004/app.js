3d7cb410a70c37ebbdc909198d59b0cabd4bb507
ViniSestari <vinifujisestari@gmail.com>
2025-09-16 22:22:40 -0300

    Add exercício 004

---
+++ Exercício004/app.js
@@ -0,0 +1,17 @@
+function fnValidarIdadeCNH(){
+    let nomeUsuario = document.getElementById('txtNome').value
+    let idadeUsuario = document.getElementById('txtIdade').value
+
+    console.log(idadeUsuario)
+
+    event.preventDefault()
+
+    if (idadeUsuario >= 18 ){
+        texto = "Parabéns " + nomeUsuario + ", você já pode tirar sua CNH!"
+        document.getElementById('resultado').innerHTML = texto
+    } else {
+        texto = "Desculpe " + nomeUsuario + ", você ainda não pode tirar sua CNH!"
+        document.getElementById('resultado').innerHTML = texto
+    }
+ }
+
