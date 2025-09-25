1d9f8dabbc489a087ec12c79ba61e498a80c3c73
ViniSestari <vinifujisestari@gmail.com>
2025-09-25 20:43:26 -0300

    Add

---
+++ Exercício008/app.js
@@ -0,0 +1,19 @@
+function fnCalcular() {
+    let valor1=0, valor2=0, operacao="", resultado=0
+    valor1 = parseFloat(document.getElementById('valor1').value)
+    valor2 = parseFloat(document.getElementById('valor2').value)
+
+    if (document.getElementById('adicao').checked==true) {
+        resultado = valor1 + valor2
+    }
+    if (document.getElementById('subtracao').checked==true) {
+        resultado = valor1 - valor2
+    }
+    if (document.getElementById('multiplicacao').checked==true) {
+        resultado = valor1 * valor2
+    }
+    if (document.getElementById('divisao').checked==true) {
+        resultado = valor1 / valor2
+    }
+    document.getElementById("resultado").innerHTML = resultado 
+}
