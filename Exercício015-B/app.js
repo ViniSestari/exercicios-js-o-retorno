95a1853d99f32d10ef2b8d19433d321ef327bdac
ViniSestari <vinifujisestari@gmail.com>
2025-10-21 22:13:53 -0300

    add 015

---
+++ Exercício015-B/app.js
@@ -0,0 +1,14 @@
+let pokemon1 = {
+    id: 7,
+    nome: 'Charmander',
+    tipo: 'Fogo',
+    altura: 0.6,
+    peso: 8.5
+}
+let pokemon2 = {
+    id: 30,
+    nome: 'Ninetales',
+    tipo: 'Fogo',
+    altura: 1.1,
+    peso: 19.9
+}
---
+++ Exercício015/app.js
@@ -0,0 +1,42 @@
+let contatos = []
+
+function fnSalvar(){
+let umContato = {
+    nome: document.getElementById('nome').value,
+    telefone: document.getElementById('telefone').value,
+    email: document.getElementById('email').value,
+    foto: document.getElementById('foto').value
+}
+
+contatos.push(umContato)
+}
+
+function fnListar() {
+    let dados = "";
+ 
+    contatos.forEach(function (umContato, i) {
+        dados += `
+        <div class="col-md-4 mb-4">
+            <div class="card" style="width: 100%;">
+                <img src="${umContato.foto}" class="card-img-top" alt="Foto de ${umContato.nome}" style="height: 200px; object-fit: cover;">
+                <div class="card-body">
+                    <h5 class="card-title">${umContato.nome}</h5>
+                    <p class="card-text">
+                        <strong>Telefone:</strong> ${umContato.telefone} -
+                        <strong>Email:</strong> ${umContato.email}
+                    </p>
+                    <a href="mailto:${umContato.email}" class="btn btn-danger">Excluir</a>
+                </div>
+            </div>
+        </div>`;
+    });
+ 
+    document.getElementById("listadeContatos").innerHTML = dados;
+}
+
+function fnExcluir(){}
+
+document.getElementById('btSalvar').addEventListener('click', function(){
+    fnSalvar()
+    fnListar()
+})
